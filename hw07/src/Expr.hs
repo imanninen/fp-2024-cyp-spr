2a283a706bdf4d99fdf60ce41ca836e1f774dc80
imanninen <igor.manninen@gmail.com>
2024-04-02 23:39:25 +0300

    fix Expr State bug + add tests

--- hw07/src/Expr.hs
+++ hw07/src/Expr.hs
@@ -1,4 +1,4 @@
-module Expr where 
+module Expr where
 import StateDemo ( State, execState, get, modify )
 import Data.Maybe ( fromJust )
 import Error (Error(..))
@@ -10,59 +10,62 @@ data BinOpeation =
   Div |
   Pow deriving (Show, Eq)
 
-data UnoOperation = 
+data UnoOperation =
   Sqrt deriving (Show, Eq)
 
 data Expr a = Var String |
   Const a |
   Uno UnoOperation (Expr a) |
   Bin BinOpeation (Expr a) (Expr a) deriving (Show, Eq)
-        
+
 type ExprState a = [(String, a)]
 
 performeOperation:: (a -> a -> a) -> Either Error a -> Either Error a -> Either Error a
 performeOperation op a b =
-  case (a, b) of 
+  case (a, b) of
     (Left er, _) -> Left er
     (_, Left er) -> Left er
     (Right x, Right y) -> Right $ op x y
 
 eval ::(Ord a, Floating a, Show a) => Expr a -> State (ExprState a) (Either Error a)
-eval (Var v) = do 
-  env <- get 
-  return $ Right $ fromJust $ lookup v env
+eval (Var v) = do
+  env <- get
+  let res = lookup v env
+  case res of
+    Just a -> return $ Right a
+    _ -> return $ Left $ UndefVar v
 
 
-eval (Const x) = return $ Right x 
+eval (Const x) = return $ Right x
 
-eval (Bin Plus x y) = do 
-  x <- eval x 
+eval (Bin Plus x y) = do
+  x <- eval x
   y <- eval y
   return $ performeOperation (+) x y
 
-eval (Bin Minus x y) = do 
-  x <- eval x 
+eval (Bin Minus x y) = do
+  x <- eval x
   y <- eval y
   return $ performeOperation (-) x y
 
-eval (Bin Mult x y) = do 
-  x <- eval x 
+eval (Bin Mult x y) = do
+  x <- eval x
   y <- eval y
   return $ performeOperation (*) x y
 
-eval (Bin Div x y) = do 
-  x <- eval x 
+eval (Bin Div x y) = do
+  x <- eval x
   y <- eval y
-  case (x, y) of 
+  case (x, y) of
     (Left er, _) -> return $ Left er
     (_, Left er) -> return $ Left er
     (Right a, Right 0) -> return $ Left $ DivByZero $ show a
     (Right a, Right b) -> return $ Right $ a / b
 
-eval (Bin Pow x y) = do 
-  x <- eval x 
+eval (Bin Pow x y) = do
+  x <- eval x
   y <- eval y
-  case (x, y) of 
+  case (x, y) of
     (Left er, _) -> return $ Left er
     (_, Left er) -> return $ Left er
     (Right 0, Right 0) -> return $ Left $ ZeroPowZero "0 ^ 0"
@@ -81,12 +84,11 @@ eval (Uno Sqrt exp) = do
   res <- eval exp
   case res of
     Left err -> return $ Left err
-    Right a -> 
-      if a < 0 
-        then return $ Left $ NegativeRoot $ show a 
-      else 
+    Right a ->
+      if a < 0
+        then return $ Left $ NegativeRoot $ show a
+      else
         return $ Right $ sqrt a
 
-runEval :: (Ord a, Floating a, Show a) => Expr a -> Either Error a
-runEval expr = 
-  execState (eval expr) [] 
+runEval :: (Ord a, Floating a, Show a) => Expr a -> [(String, a)] -> Either Error a
+runEval expr = execState (eval expr)
--- hw07/test/Spec.hs
+++ hw07/test/Spec.hs
@@ -1,13 +1,13 @@
 import Test.Tasty (defaultMain, testGroup, TestTree)
 import Parser (runParser, parseExpression)
 import Test.Tasty.HUnit ((@?=), testCase, assertBool, assertFailure)
-import Expr (Expr(..), BinOpeation(..), UnoOperation(..))
+import Expr (Expr(..), BinOpeation(..), UnoOperation(..), runEval)
 import Error (Error(..))
 
 
 testParser :: TestTree
 testParser = 
-    testGroup "ParserTests" [baseDefTests, simpleTests, trikyTests]
+    testGroup "Parser tests" [baseDefTests, simpleTests, trikyTests]
   where
     { 
     baseDefTests = testGroup "Base def tests" [
@@ -24,7 +24,6 @@ testParser =
       , testCase "+ 123 * 45 6 == Bin (+) 123 (Bin (*) 45 6)" $ runParser parseExpression "+ 123 * 45 6" @?= Right ("", Bin Plus (Const 123) (Bin Mult (Const 45) (Const 6)))
       , testCase "+ * 123 45 6 == Bin (+) (Bin (*) 123 45) 6" $ runParser parseExpression "+ * 123 45 6" @?= Right ("", Bin Plus (Bin Mult (Const 123) (Const 45)) (Const 6))
       , testCase "/ sqrt 123 xyz == Bin (/) (Sqrt 123) xyz" $ runParser parseExpression "/ sqrt 123 xyz" @?= Right ("", Bin Div (Uno Sqrt (Const 123)) (Var "xyz"))
-
     ];
     trikyTests = testGroup "Triky tests" [
         testCase "sqrt sqrt == Error" $ runParser parseExpression "sqrt sqrt" @?= Left (ParserErr "digit expected!")
@@ -33,5 +32,27 @@ testParser =
     ]
     }
 
+evalEnv :: [(String, Double)]
+evalEnv = [("x", 42), ("y", 39), ("two", 2)]
+
+testEval :: TestTree
+testEval = testGroup "Eval tests" [baseTests, simpleTests]
+  where {
+    baseTests = testGroup "Base tests" [
+      testCase "Const 2 == 2" $ runEval (Const 2) evalEnv @?= Right 2
+      , testCase "Var x == 42" $ runEval (Var "x") evalEnv @?= Right 42
+      , testCase "Var z == Error" $ runEval (Var "z") evalEnv @?= Left (UndefVar "z")
+    ];
+    simpleTests = testGroup "Simple tests" [
+      testCase "(two + 2) * 2 == 8" $ runEval (Bin Mult (Bin Plus (Var "two") (Const 2)) (Const 2)) evalEnv @?= Right 8
+      , testCase "two * 2 == 4" $ runEval (Bin Mult (Var "two") (Const 2)) evalEnv @?= Right 4
+      , testCase "two / 2 == 1" $ runEval (Bin Div (Var "two") (Const 2)) evalEnv @?= Right 1
+      , testCase "two - 2 == 0" $ runEval (Bin Minus (Var "two") (Const 2)) evalEnv @?= Right 0
+      , testCase "two ^ 2 == 2" $ runEval (Bin Pow (Var "two") (Const 2)) evalEnv @?= Right 4
+      , testCase "two / 0 == Error" $ runEval (Bin Div (Var "two") (Const 0)) evalEnv @?= Left (DivByZero "2.0")
+      , testCase "sqrt -1 == Error" $ runEval (Uno Sqrt (Bin Minus (Const 0) (Const 1))) evalEnv @?= Left (NegativeRoot "-1.0")
+    ]
+    }
+
 main :: IO ()
-main = defaultMain $ testGroup "Tests" [testParser]
+main = defaultMain $ testGroup "Tests" [testParser, testEval]
